1ec3951badfeb74c1310d6707d07c8a107c05591
NathanielStirret <102550124+NathanielStirret@users.noreply.github.com>
2026-03-12 15:23:21 +0000

    Create LIS.py

---
+++ LIS.py
@@ -0,0 +1,16 @@
+def lis(arr):
+    n = len(arr)
+    if n == 0:
+        return 0
+
+    dp = [1] * n   # this is our memory
+
+    # Fill the memeory
+    for i in range(1, n):
+        for j in range(i):
+            if arr[j] < arr[i]:
+                # save the best result so far
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    # The answer is the maximum saved value
+    return max(dp)
